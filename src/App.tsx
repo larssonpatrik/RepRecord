cacd276944056c7742ff101542bd3886e8c45788
Patrik Larsson <patrik@dictodesigns.com>
2023-02-24 14:19:01 +0100

    testing out data structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import { exerciseData } from "./exerciseData";
 import "./App.css";
 
 function App() {
-  return <h1>hello world!</h1>;
+  const [exerciseBank, setExerciseBank] =
+    useState<{ exerciseID: number; name: string }[]>(exerciseData);
+  const [workoutPrograms, setWorkoutPrograms] = useState<
+    { exerciseID: number; weight: number; sets: number; reps: number }[]
+  >([]);
+
+  return (
+    <div style={{ display: "flex" }}>
+      <div>
+        <h1>TEST</h1>
+        {exerciseBank.map((exercise) => {
+          return (
+            <div>
+              <h2>{exercise.name}</h2>
+              <p>id: {exercise.exerciseID}</p>
+              <button
+                onClick={() => {
+                  setWorkoutPrograms([
+                    ...workoutPrograms,
+                    {
+                      exerciseID: exercise.exerciseID,
+                      weight: 1,
+                      sets: 3,
+                      reps: 8,
+                    },
+                  ]);
+                }}
+              >
+                Add to workout program
+              </button>
+            </div>
+          );
+        })}
+      </div>
+      <div>
+        <h1>TEST</h1>
+        {workoutPrograms.map((workoutProgram) => {
+          return (
+            <div>
+              <h1>
+                {
+                  exerciseBank.find(
+                    (exercise) =>
+                      exercise.exerciseID === workoutProgram.exerciseID
+                  )?.name
+                }
+              </h1>
+              <button>Test</button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/exerciseData.ts
@@ -0,0 +1,18 @@
+type exerciseDataType = { exerciseID: number; name: string }[];
+
+export const exerciseData: exerciseDataType = [
+  { exerciseID: 1, name: "Bench Press" },
+  { exerciseID: 2, name: "Dumbbell Flyes" },
+  { exerciseID: 3, name: "High Barbell Row" },
+  { exerciseID: 4, name: "Hammer Curls" },
+  { exerciseID: 5, name: "Dumbbell Shoulder Press" },
+  { exerciseID: 6, name: "Lateral Cable Raise" },
+  { exerciseID: 7, name: "Facepulls" },
+  { exerciseID: 8, name: "Tricep Cable" },
+  { exerciseID: 9, name: "Bicep Cable" },
+  { exerciseID: 10, name: "Tricep Cable" },
+  { exerciseID: 11, name: "Rotator Cuff Cable" },
+  { exerciseID: 12, name: "Barbell Shoulder Press" },
+  { exerciseID: 13, name: "Deadlift" },
+  { exerciseID: 14, name: "Barbell Row" },
+];
